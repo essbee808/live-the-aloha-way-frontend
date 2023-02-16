5d2364f344b250e8ea30d6139c646aee6cfd6ab9
Sheena Bonilla <sheena.myb@gmail.com>
2023-02-16 00:05:04 -0800

    Install emailjs and modify contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import {useRef} from 'react';
+import emailjs from 'emailjs-com'
 
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+
 
 const ContactForm = () => {
   const form = useRef();
@@ -17,12 +20,18 @@ const ContactForm = () => {
   }
   
   return (
-    <div>
-        <p>If you know of a child or family in need of a meal, groceries, school supplies,
-            toiletries or necessities, or if you yourself are in need, please send us a message
-            and we will do our best to share our blessings and ALOHA.
-        </p>
-    </div>
+    <Container>
+       <Row>
+            <Col>
+            <h3 className="fs-2 fw-bold"><center>Get in Touch</center></h3>
+            <Form ref={form} onSubmit={sendEmail}>
+                <Form.Group className="mb-3" controlId="Form.ControlInput1">
+                    <Form.Control type="text" name="first_name" placeholder="First Name" required/>
+                </Form.Group>
+            </Form>
+            </Col>
+       </Row>
+    </Container>
   )
 }
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,6 +5,10 @@ const Contact = () => {
   return (
     <section>
         <h1>Contact Page</h1>
+        <p>If you know of a child or family in need of a meal, groceries, school supplies,
+            toiletries or necessities, or if you yourself are in need, please send us a message
+            and we will do our best to share our blessings and ALOHA.
+        </p>
         <ContactForm/>
     </section>
   )
