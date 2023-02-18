9275097274ca2d3dfac55f4e1e285b1aa508a35c
Sheena Bonilla <sheena.myb@gmail.com>
2023-02-17 22:37:56 -0800

    Upload transparent logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -15,9 +15,10 @@ const Footer = () => {
             <div className="footer-menu">
                 <ul className="f-menu">
                     <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
                     <li><a href="/gallery">Gallery</a></li>
-                    <li><a href="/share-aloha">Share Aloha</a></li>
-                    <li><a href="/contact">Contact Us</a></li>
+                    <li><a href="/share-aloha">Shop</a></li>
+                    <li><a href="/contact">Contact</a></li>
                 </ul>
             </div>
             <p>Copyright &copy; Live The Aloha Way. All Rights Reserved.</p>
