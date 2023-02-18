0f510752fd2723befc296f1c9cbc781776c1d2ca
Sheena Bonilla <sheena.myb@gmail.com>
2023-02-17 20:59:48 -0800

    Edit footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
                     <li><a href="/">Home</a></li>
                     <li><a href="/gallery">Gallery</a></li>
                     <li><a href="/share-aloha">Share Aloha</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                    <li><a href="/contact">Contact Us</a></li>
                 </ul>
             </div>
         </div>
